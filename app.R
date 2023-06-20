fbd3a0347ba5b755b036eb581127def86b95b63c
Anushka Siwakoti <siwakoti@battelleecology.org>
2023-06-20 09:21:39 -0600

    update

--- app.R
+++ app.R
@@ -1,42 +1,20 @@
-<<<<<<< HEAD
 #app.R
-
-# used to establish the API
-# how its hosted on the server and funnels all incoming 
-# requests to the plumer.R file
 library(plumber)
-library(tidyverse)
-library(lme4)
-library(jsonlite)
+library(dplyr)
 library(httr)
+library(jsonlite)
+library(glue)
+library( rlang)
+library(data.table)
+library(rlist)
+library(stringr)
 
 port <- Sys.getenv('PORT')
 
 pr <- plumb("plumber.R")
 
 pr$run(
-  # host NEEDS to be 0.0.0.0
   host = '0.0.0.0',
   port = as.numeric(port)
-=======
-#app.R
-
-# used to establish the API
-# how its hosted on the server and funnels all incoming 
-# requests to the plumer.R file
-library(plumber)
-library(tidyverse)
-library(lme4)
-library(jsonlite)
-library(httr)
-
-port <- Sys.getenv('PORT')
-
-pr <- plumb("plumber.R")
-
-pr$run(
-  # host NEEDS to be 0.0.0.0
-  host = '0.0.0.0',
-  port = as.numeric(port)
->>>>>>> e2a294c15498949c17eb4f5fc111e4d4887702ee
 )
+
--- init.R
+++ init.R
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 # init.R
+#
 
-# installs libraries onto the Heroku server
-
-my_packages = c( 'plumber', "tidyverse", "lme4", 'jsonlite', 'httr')
+my_packages = c( 'plumber', 'dplyr', 'jsonlite', 'httr', 'glue', 'rlang', 'data.table', 'rlist', 'stringr')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -11,18 +9,4 @@ install_if_missing = function(p) {
   }
 }
 
-=======
-# init.R
-
-# installs libraries onto the Heroku server
-
-my_packages = c( 'plumber', "tidyverse", "lme4", 'jsonlite', 'httr')
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
->>>>>>> e2a294c15498949c17eb4f5fc111e4d4887702ee
 invisible( sapply( my_packages, install_if_missing))
