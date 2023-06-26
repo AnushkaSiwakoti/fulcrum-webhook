6e8c509c21405bc3a7b46e600e9fbb0ec7da6359
AnushkaSiwakoti <134111960+AnushkaSiwakoti@users.noreply.github.com>
2023-06-26 11:38:26 -0600

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -17,14 +17,16 @@ function(pr) {
   pr %>%
     pr_filter('scs-dev', function(req, res) {
       # Retrieve the record ID from the request body
-      record <- jsonlite::fromJSON(req$postBody)
-      record_id <- record$data$id
+      record_json <- jsonlite::toJSON((req$postBody)
+      shipment <- jsonlite::fromJSON(record_json)
+      #record <- jsonlite::fromJSON(req$postBody)
+      record_id <- shipmentd$data$id
       
       api_token <- Sys.getenv('FULCRUM_API_NEON')
-      shipment <- get_record(record_id, api_token)
+      #shipment <- get_record(record_id, api_token)
       
       # Check if shipment manifest is "yes" and no attachment ID exists
-      if (shipment$data$form_values$`0ce0` == "yes" && is.null(shipment$data$form_values$`cf80`)) {
+      if (!is.null(shipment$data$form_values$`0ce0`) && shipment$data$form_values$`0ce0` == "yes" && is.null(shipment$data$form_values$`cf80`)) {
         if (is.null(req$postBody)) {
           res$status <- 404
           return(list(error = "Body not found"))
