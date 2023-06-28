a7005dfdd2f5d44de21184db328df5d66ac1f707
AnushkaSiwakoti <134111960+AnushkaSiwakoti@users.noreply.github.com>
2023-06-28 09:07:01 -0600

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -16,7 +16,7 @@ source('fulcrum_file_upload.R')
 function(pr) {
   pr %>%
     pr_filter('scs-dev', function(req, res) {
-       print(paste0('>>>>>>>>>>>>LOGGER ACTIVE<<<<<<<<<<<<<<<', Sys.time()))
+      print(paste0('>>>>>>>>>>>>LOGGER ACTIVE<<<<<<<<<<<<<<<', Sys.time()))
       #print( ls.str(env=req))  #this is used to checkout the request that is being posted to the webhook
       cat( as.character( Sys.time()), "-",
            req$REQUEST_METHOD, req$PATH_INFO, "-",
@@ -26,105 +26,71 @@ function(pr) {
         res$status <- 404
         return(list(error = "Body not found"))
       } else {
-        print("Creating draft manifest...")
+        print('payload found')
         
-        body <- req$postBody
-        record <- fromJSON(body)
+        record <- jsonlite::parse_json( req$postBody)
         
-        record_id <- record$id
         
-        record_json <- jsonlite::toJSON(record)
-        record <- jsonlite::fromJSON(record_json)
-        shipment <- jsonlite::fromJSON(record_json)
         
-        api_token <- Sys.getenv('FULCRUM_API_NEON')
-        
-        # Check if shipment manifest is "yes" and no attachment ID exists
-        if (!is.null(record$record$form_values$`0ce0`) && record$record$form_values$`0ce0` == "yes" && is.null(record$record$form_values$`cf80`)) {
-          print("Creating draft manifest...")
+        if( !is.null( record$type) && record$type %in% c('record.create', 'record.update') ){
+          record_id <- record$id
+          form_id <- record$data$form_id
+          print(paste0('recordID = ',record_id))
+          print(paste0('formID = ',form_id))   
           
-          # Get the sample array from the shipment creation record
-          sample_array <- unlist(record$record$form_values$`88f2`)
-          #print(grepl("storageCode", sample_array))
-          
-          # if (any(grepl("storageCode", sample_array))) {
-          #   print(">>>> STORAGE CODE DETECTED >>>>>>>")
-          
-          # Parse the sample array
-          sample_array <- strsplit(sample_array, "|", fixed = TRUE)[[1]]
-          array_parse <- list()
-          for (s in 1:length(sample_array)) {
-            sample <- jsonlite::parse_json(sample_array[s])
-            array_parse <- list.append(array_parse, sample)
-          }
-          
-          # Get unique sample types
-          all_types <- unique(lapply(array_parse, `[[`, "sampleclass"))
-          
-          # Prepare to store inventory samples
-          inventory_samples <- list()
-          
-          # Iterate over sample types
-          for (t in 1:length(all_types)) {
-            samp_list <- array_parse[lapply(array_parse, `[[`, "sampleclass") == unlist(all_types[t])]
-            samp_filter <- samp_list[!sapply(samp_list, is.null)]
-            samp_po <- names(shipment)[names(shipment) %in% names(samp_filter[[1]])]
-            sample_mapping <- get_record(all_types[t], api_token)
+          if( !is.null(form_id) && form_id == "555deee5-7e4d-4558-8101-148dfa06d870"){
+            print("Shipment creation payload found")
             
-            # Iterate over sample mappings
-            for (m in 1:length(sample_mapping$rows)) {
-              map <- sample_mapping$rows[[m]]
-              map_filter <- map[names(map) %in% samp_po]
-              map_filter$sampleclass <- NULL
-              fields <- unname(unlist(map_filter))
-              query_fields <- paste(fields, collapse = ",")
-              appname <- stringr::str_replace_all(map$appname, " ", "%20")
+            
+            # Check if shipment manifest is "yes" and no attachment ID exists
+            if (!is.null(record$data$form_values$`0ce0`) && record$data$form_values$`0ce0` == "yes" && is.null(record$data$form_values$`cf80`)) {
+              print("Creating draft manifest...")
+              api_token <- Sys.getenv('FULCRUM_API_NEON')
               
-              if (!is.null(map$repeatablename)) {
-                repeatablename <- stringr::str_split(map$repeatablename, "/")[[1]]
-                repeatablename <- repeatablename[length(repeatablename)]
+              # Get the sample array from the shipment creation record
+              sample_array <- unlist(record$data$form_values$`88f2`)
+              
+              # Parse the sample array
+              sample_array <- strsplit(sample_array, "|", fixed = TRUE)[[1]]
+              array_parse <- list()
+              for (s in 1:length(sample_array)) {
+                sample <- jsonlite::parse_json(sample_array[s])
+                sample <- rmNullObs(sample)
+                array_parse <- list.append(array_parse, sample)
+              }
+              
+              if (length(array_parse) > 0) {
+                draft_manifest <- data.frame(matrix(unlist(array_parse), nrow = length(array_parse), byrow = TRUE), stringsAsFactors = FALSE)
+                colnames(draft_manifest) <- names(array_parse[[1]])
+                draft_manifest_filename <- glue::glue("{record$data$form_values['de4e']}_draft_manifest.csv")
+                write.csv(draft_manifest, file = draft_manifest_filename, row.names = FALSE)
+                form_values <- record$data
+                filepath <- draft_manifest_filename
+                attachment_key <- "cf80"
+                uploadFile(api_token, record_id, filepath, form_values, attachment_key)
+                print("Draft manifest created and uploaded.")
+                
+                return(list(status = 200))
               } else {
-                repeatablename <- map$repeatablename
+                print("Draft manifest not created and uploaded.")
+                return(list(status = 500))
               }
-              
-              if (length(purrr::compact(lapply(samp_list, `[[`, "sample_barcode"))) == length(samp_list)) {
-                id_field <- "sample_barcode"
-              } else {
-                id_field <- "sample_tag"
-              }
-              
-              parent_data <- get_record(map$fulcrumcode)
-              
-              if (length(parent_data$rows) > 0) {
-                for (row in 1:length(parent_data$rows)) {
-                  check_data <- parent_data$rows[[row]]
-                  if (exists("check_data") && !is.null(check_data)) {
-                    sample <- list()
-                    for (field in fields) {
-                      sample[[field]] <- check_data[[field]]
-                    }
-                    inventory_samples <- rlist::list.append(inventory_samples, sample)
-                  }
-                }
-              }
+            }else{
+              print('draft manifest not requested')
+              res$status <- 204
+              return( list( error="MANIFEST NOT REQUESTED"))
             }
+          }else{
+            ## if it's a record.create or record.update but NOT the target app/form, still return a response
+            print('payload not from shipment creation')
+            res$status <- 204
+            return( list( error="NOT SHIPMENT CREATION RECORD"))
           }
-          
-          if (length(inventory_samples) > 0) {
-            draft_manifest <- data.frame(matrix(unlist(inventory_samples), nrow = length(inventory_samples), byrow = TRUE), stringsAsFactors = FALSE)
-            colnames(draft_manifest) <- names(inventory_samples[[1]])
-            draft_manifest_filename <- glue::glue("{shipment$data$shipment_id}_draft_manifest.csv")
-            write.csv(draft_manifest, file = draft_manifest_filename, row.names = FALSE)
-            form_values <- get_record(record_id, api_token)
-            filepath <- draft_manifest_filename
-            attachment_key <- "cf80"
-            uploadFile(api_token, record_id, filepath, form_values, attachment_key)
-            print("Draft manifest created and uploaded.")
-            return(list(status = 200))
-          } else {
-            print("Draft manifest not created and uploaded.")
-            return(list(status = 500))
-          }
+        }else{
+          ## if there's another type of webhook message (e.g. record.delete), still return a response
+          print('record type incorrect')
+          res$status <- 204
+          return( list( error="NO RECORD CREATE OR UPDATE"))
         }
       }
     })
