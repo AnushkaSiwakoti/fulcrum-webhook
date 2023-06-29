16c804c125960a8547fcfcc0c00a9be52b3e2fe8
AnushkaSiwakoti <134111960+AnushkaSiwakoti@users.noreply.github.com>
2023-06-29 07:47:06 -0600

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -3,14 +3,14 @@ library(plumber)
 library(jsonlite)
 library(dplyr)
 library(glue)
+library(rlist)
 library(data.table)
 library(httr)
 library(stringr)
-library(rlist)
 
 # Source the required files
-
 source('functions.R')
+
 
 #* @plumber
 function(pr) {
