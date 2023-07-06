4a7d6d7a21ce6d78800592e7476ee01711cdc753
AnushkaSiwakoti <134111960+AnushkaSiwakoti@users.noreply.github.com>
2023-07-06 14:36:14 -0600

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
 
 
-uploadFile <- function(api_token = Sys.getenv('FULCRUM_API_NEON'),recordid,filepath,form_values,attachment_key){
+uploadFile <- function(api_token,recordid,filepath,form_values,attachment_key){
   require(httr)
   require(jsonlite)
   url <- paste0("https://api.fulcrumapp.com/api/v2/attachments")
@@ -12,7 +12,7 @@ uploadFile <- function(api_token = Sys.getenv('FULCRUM_API_NEON'),recordid,filep
     }
   ]
 }')
-  
+
   request <- httr::POST(url,
                         config = add_headers("X-ApiToken" = api_token,
                                              Accept = "application/json",
@@ -20,28 +20,28 @@ uploadFile <- function(api_token = Sys.getenv('FULCRUM_API_NEON'),recordid,filep
   #print(request)
   content <- httr::content(request, as = 'parsed')
   #content <- content(request)
-  
+
   url <- content$url
   id <- content$id
-  
-  
-  
+
+
+
   # put file in box
   body <- split(x = filepath, f = id, drop = FALSE)
   print(body)
   url <- paste0(url)
-  
+
   request <- httr::PUT(url,
                        config = add_headers(
                          Accept = "application/json",
                          'Content-Type' = "application/json"), body = body, encoding = "json")
   #content <- httr::content(request, as = 'parsed')
-  
-  
-  
+
+
+
   # finalize post
   url <- paste0("https://api.fulcrumapp.com/api/v2/attachments/finalize")
-  
+
   body <- paste0('{
   "owners": [
     {
@@ -51,7 +51,7 @@ uploadFile <- function(api_token = Sys.getenv('FULCRUM_API_NEON'),recordid,filep
   ],
   "ID": "',id,'"
 }')
-  
+
   #print(body)
   request <- httr::POST(url,
                         config = add_headers("X-ApiToken" = api_token,
@@ -61,42 +61,44 @@ uploadFile <- function(api_token = Sys.getenv('FULCRUM_API_NEON'),recordid,filep
   if (status_code(request) == 201) {
     # Success - AWS box created
     print("Created")
-    
+
   }
-  
-  
-  
+
+
+
   #put file attachment
-  
+  # Get the record
+  # record <- get_record(recordid, api_token)
+  #attachment_key <- 'cf80'
+  #form_values <- record$record$form_values
+  #filepath <- 'C:/Users/siwakoti/Documents/fulcrumWebhook/fulcrum-webhook/D1420200618084715169_draft_manifest.csv'
+
+  form_values[[attachment_key]] <- c(form_values[[attachment_key]],list( list(name = basename(filepath), attachment_id = id)))
+
+  # PUT request to update the record
   url <- paste0("https://api.fulcrumapp.com/api/v2/records/", recordid, ".json")
-  
-  #form_values[[attachmentkey]] <- list(list(name = basename(filepath), attachment_id = id))
-  
-  attachment_value <- list(
-      list(
-        name = basename(filepath),
-        attachment_id = id
-      )
-    )
-  
-  form_values[attachment_key] <- attachment_value
 
   body <- toJSON(list(record = list(form_values = form_values)), auto_unbox = TRUE)
-  
-  
-  
+  print(body)
+
   request <- httr::PUT(url,
                        config = add_headers("X-ApiToken" = api_token,
                                             Accept = "application/json",
-                                            "Content-Type" = "application/json"), body = body, encode = "json")
-  
-  
+                                            "Content-Type" = "application/json"),
+                       body = body,
+                       encode = "json")
+
+  if (status_code(request) == 200) {
+    # Success - Record updated
+    print("Record updated successfully.")
+  } else {
+    # Error updating record
+    print("Error updating record.")
+  }
   content <- httr::content(request, as = 'parsed')
-  
-  
+
   return(content)
 }
-
 
 
 #get records function 
